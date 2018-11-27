7b8f57d896b4322603f0ecef6118c8b1f934a573
helei <heleihg@163.com>
2018-11-27 14:27:42 +0800

    9-1 Zuul：Pre和Post过滤器 9-2 Zuul：限流

--- src/main/java/com/imooc/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/imooc/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.imooc;
+package com.imooc.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/imooc/apigateway/exception/RateLimitException.java
@@ -0,0 +1,7 @@
+package com.imooc.apigateway.exception;
+
+/**
+ * Created by helei on 2018-11-26.
+ */
+public class RateLimitException extends RuntimeException {
+}
