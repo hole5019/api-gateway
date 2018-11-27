4f1012abd28ae3b28fffed6b48802a9821d964f8
helei <heleihg@163.com>
2018-11-27 17:40:10 +0800

    9-9 Zuul：跨域

---
+++ src/main/java/com/imooc/apigateway/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.imooc.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+/**
+ * 跨域配置
+ * Created by helei on 2018-11-27.
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setMaxAge(300l);
+
+        source.registerCorsConfiguration("/**",config);
+        return new CorsFilter(source);
+    }
+}
